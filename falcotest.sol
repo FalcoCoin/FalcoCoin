600e0cfd50008258c425613240bf0629d735fff8
FalcoCoin <93152903+FalcoCoin@users.noreply.github.com>
2021-10-25 19:08:42 +0100

    Add files via upload

--- falcotest.sol
+++ falcotest.sol
@@ -300,11 +300,11 @@ library Address {
      */
     function isContract(address account) internal view returns (bool) {
         // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
-        // and 0x5954174A6f56F24130066b21a9D866E3D348f363 is returned
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
         // for accounts without code, i.e. `keccak256('')`
         bytes32 codehash;
         bytes32 accountHash =
-            0x5954174A6f56F24130066b21a9D866E3D348f363;
+            0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
         // solhint-disable-next-line no-inline-assembly
         assembly {
             codehash := extcodehash(account)
